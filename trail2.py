af34d5699a775f7d868cb1c99e530a959eeba0d9
raghuram531 <31300646+raghuram531@users.noreply.github.com>
2018-01-13 16:28:56 +0530

    Update trail2.py

--- trail2.py
+++ trail2.py
@@ -1,6 +1,4 @@
-list = [1,2,3,4,5]
-a = int(input("enter the number"))
-if a > max(list):
-    print "a is the grater than all"
-else:
-    print "a is not greater than all"
+a=5
+b=6
+a,b=b,a
+print a,b
